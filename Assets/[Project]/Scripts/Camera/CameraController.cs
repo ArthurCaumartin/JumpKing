cdb967bed5199ef8e652961d8637dc45d7f23baa
Pouletator <loupsergent513@gmail.com>
2025-01-30 17:32:21 +0100

    camera controller et tout

---
+++ Assets/[Project]/Scripts/Camera/CameraController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+    [SerializeField] float vitesse;
+    [SerializeField] Vector3 target;
+    [SerializeField] List<CameraNode> CameraNodesList;
+    [SerializeField] Transform Player;
+    [SerializeField] Transform InterfaceCameraTarget;
+    CameraNode cameranode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameranode = GetNearestNode();
+        target = cameranode.transform.position;
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, target + new Vector3(0, -GetOffSet(),-10), Time.deltaTime * vitesse);
+    }
+
+    CameraNode GetNearestNode()
+    {
+        CameraNode nodetoreturn = null;
+        float minDistance = Mathf.Infinity;
+        foreach (var item in CameraNodesList)
+        {
+           float Distance = Vector2.Distance(item.transform.position, Player.position);
+           if(Distance < minDistance)
+            {
+                minDistance = Distance;
+                nodetoreturn = item;
+            }
+        }
+        return nodetoreturn;
+    }
+
+    public float GetOffSet()
+    {
+        Vector3 worldScreenCenter = _camera.ScreenToWorldPoint(new Vector2(_camera.pixelWidth, _camera.pixelHeight)/2);
+        return Vector2.Distance(_camera.ScreenToWorldPoint(InterfaceCameraTarget.position), worldScreenCenter);
+    }
+}
